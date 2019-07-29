81121e2e319b3c4cacfc35b30a702d21486fbf23
Malak Tayeh <malaktayeh@outlook.com>
2019-07-28 20:35:56 -0400

    added hover styling for services icons

--- src/components/Offerings/index.js
+++ src/components/Offerings/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import styles from './styles.module.scss'
 
 const Offerings = ({ gridItems }) => (
   <div className='columns is-centered is-mobile is-multiline'>
     {gridItems.map(item => (
-      <div key={item.image} className='column is-full-mobile is-half-tablet is-one-third-desktop is-one-quarter-fullhd' style={{borderRadius: '5px'}}>
+      <div key={item.image} className='column is-full-mobile is-half-tablet is-one-third-desktop is-one-quarter-fullhd' >
         <section className='section'>
-          <Link to={item.link}>
-            <p className='has-text-centered has-text'>
-              <img alt='' src={item.image} />
-            </p>
-            <p className='has-text-weight-semibold has-text-centered'>{item.text}</p>
-          </Link>
+          <div className={styles.changeColorOnHover}>
+            <Link to={item.link}>
+              <p className='has-text-centered has-text'>
+                <img alt={item.text} src={item.image} />
+              </p>
+              <p className={`has-text-weight-semibold has-text-centered has-text-grey`}>{item.text}</p>
+            </Link>
+          </div>
         </section>
       </div>
     ))}
